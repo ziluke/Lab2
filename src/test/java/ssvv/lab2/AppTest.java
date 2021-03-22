ed30086e62bcff7f674b0ff8b0e340d30015c6af
ziluke <lukasvictor99@yahoo.com>
2021-03-22 15:43:38 +0200

    MAde 2 test-cases;

--- src/test/java/ssvv/lab2/AppTest.java
+++ src/test/java/ssvv/lab2/AppTest.java
@@ -1,8 +1,20 @@
 package ssvv.lab2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 /**
  * Unit test for simple App.
@@ -16,5 +28,41 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void tc_1_addStudent(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "src/test/studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "src/test/teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "src/test/note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        service.saveStudent("33", "New Student 3", 934);
+
+        Student aux = fileRepository1.findOne("33");
+
+        assertEquals("New Student 3", aux.getNume());
+    }
+
+    @Test
+    public void tc_2_addStudent(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "src/test/studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "src/test/teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "src/test/note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        int aux = service.saveStudent("33", "New Student 3", 934);
+
+        assertEquals(0, aux);
     }
 }
